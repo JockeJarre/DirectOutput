521d9abff78e7f70b7198b73154c0e7538132b19
Jarr3 <Richard.Ludwig+gh@gmail.com>
2025-10-06 08:22:36 +0200

    Fix WiX 6 installer creation

--- DOFSetupB2SFixup/B2SFixup.cs
+++ DOFSetupB2SFixup/B2SFixup.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
 using WixToolset.Dtf.WindowsInstaller;
-using System.Text.RegularExpressions;
 using System.Reflection;
 using System.Xml;
 using System.IO;
